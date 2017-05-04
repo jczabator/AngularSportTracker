204809c4bfefc07b6d6461d2dfcc84ed98ffd548
cjbg <cjbg@o2.pl>
2017-05-04 14:37:42 +0200

    add core module

---
+++ AngularSportTracker/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MaterialModule } from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+//TODO: import navigation component
+
+@NgModule({
+  imports:[],
+  exports:[],
+  declarations:[],
+  providers:[]
+})
+export class CoreModule {  
+}
